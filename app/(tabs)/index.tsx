22c07820c1d574d626d80ffadb9ccc867c04084d
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2025-05-12 03:25:06 +0300

    second day

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,50 @@
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Image, Modal, Pressable, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={{flex: 1, backgroundColor: 'plum', paddingTop: 60}}>
-      {/* <Text>BBBBBBBBBBBBBBBB</Text>
-      <Image source={require('../../assets/images/adaptive-icon.png')} style={{width: 300, height: 300}} />
-      <Image source={{ uri: 'https://picsum.photos/300' }} style={{width: 300, height: 300}} /> */}
-      <ImageBackground source={{uri: 'http://picsum.photos/300'}} style={{ flex: 1}}>
-        <Text style={{fontSize: 20, padding: 20}}>Hello World</Text>
-      </ImageBackground>
+      <Button title='Press' onPress={() => setModalVisible(true)} color={'green'} />
+      <StatusBar backgroundColor={'black'} barStyle={'light-content'} />
+      <Modal visible={modalVisible} onRequestClose={() => setModalVisible(false)}
+       animationType='slide' presentationStyle='formSheet'
+      >
+        <Text>Modal</Text>
+        <Button title='Close' onPress={() => setModalVisible(false)} color={'red'} />
+      </Modal>
+      <ScrollView>
+        <Pressable onPress={() => console.log('Image Pressed')}>
+        <Image source={require('../../assets/images/adaptive-icon.png')} style={{width: 300, height: 300}} />
+        </Pressable>
+        <Text>
+          This is a demo app for the React Native CLI. It is a simple app that shows how to use the React Native CLI to create a new app.
+          {'\n\n'}
+          This app is a simple example of how to use the React Native CLI to create a new app. It is a simple app that shows how to use the React Native CLI to create a new app.
+          {'\n\n'}
+          This app is a simple example of how to use the React Native CLI to create a new app. It is a simple app that shows how to use the React Native CLI to create a new app.
+          {'\n\n'}
+          This app is a simple example of how to use the React Native CLI to create a new app. It is a simple app that shows how to use the React Native CLI to create a new app.
+          {'\n\n'}
+          This app is a simple example of how to use the React Native CLI to create a new app. It is a simple app that shows how to use the React Native CLI to create a new app.
+          {'\n\n'}
+          This app is a simple example of how to use the React Native CLI to create a new app. It is a simple app that shows how to use the React Native CLI to create a new app.
+          {'\n\n'}
+          This app is a simple example of how to use the React Native CLI to create a new app. It is a simple app that shows how to use the React Native CLI to create a new app.
+          {'\n\n'}
+          This app is a simple example of how to use the React Native CLI to create a new app. It is a simple app that shows how to use the React Native CLI to create a new app.
+          {'\n\n'}
+          This app is a simple example of how to use the React Native CLI to create a new app. It is a simple app that shows how to use the React Native CLI to create a new app.
+          {'\n\n'}
+          This app is a simple example of how to use the React Native CLI to create a new app. It is a simple app that shows how to use the React Native CLI to create a new app.
+          {'\n\n'}
+          This app is a simple example of how to use the React Native CLI to create a new app. It is a simple app that shows how to use the React Native CLI to create a new app.
+          {'\n\n'}
+          This app is a simple example of how to use the React Native CLI to create a new app. It is a simple app that shows how to use the React Native CLI to create a new app.
+        </Text>
+        <Image source={require('../../assets/images/adaptive-icon.png')} style={{width: 300, height: 300}} />
+      </ScrollView>
+      
     </View>
   );
 }
